c7c76adc59e490b677e54fa02d720da2ae1dbceb
Saurabh Wagh <saurabhw658@gmail.com>
2025-09-29 17:07:28 +0530

    Changed the repositories interface name

--- src/main/java/com/scaler/productservice_sep2025/repositories/Categoryrepo.java
+++ src/main/java/com/scaler/productservice_sep2025/repositories/Categoryrepo.java
@@ -1,8 +1,9 @@
 package com.scaler.productservice_sep2025.repositories;
 
+import com.scaler.productservice_sep2025.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Category extends JpaRepository<Category,Long> {
+public interface Categoryrepo extends JpaRepository<Category,Long> {
 }
--- src/main/java/com/scaler/productservice_sep2025/repositories/Productrepo.java
+++ src/main/java/com/scaler/productservice_sep2025/repositories/Productrepo.java
@@ -1,8 +1,9 @@
 package com.scaler.productservice_sep2025.repositories;
 
+import com.scaler.productservice_sep2025.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Product extends JpaRepository<Product, Long> {
+public interface Productrepo extends JpaRepository<Product, Long> {
 }
